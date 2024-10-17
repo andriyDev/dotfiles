47c60b7f334d8ffcebf7593d4ee8fce78e4e3a2d
Andriy Dzikh <andriydzikh@google.com>
2024-10-17 16:13:53 -0700

    Fix error when accepting auto-completion.

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -23,6 +23,8 @@ return {
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-path',
+      'hrsh7th/cmp-vsnip',
+      'hrsh7th/vim-vsnip',
     },
     config = function()
       local cmp = require('cmp')
