3dd477a0510f9ca788189c3ddf4b6a53148b3d55
Andriy Dzikh <andriydzikh@google.com>
2024-10-23 10:14:33 -0700

    Make F12 go to definition.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -22,6 +22,7 @@ vim.keymap.set('', '<C-p>', '<cmd>Files<cr>')
 vim.keymap.set('', '<A-C-p>', '<cmd>AllFiles<cr>')
 vim.keymap.set('', '<C-S-p>', '<cmd>Commands<cr>')
 vim.keymap.set('', 'K', function() vim.lsp.buf.hover() end)
+vim.keymap.set('', '<F12>', function() vim.lsp.buf.definition() end)
 
 vim.keymap.set({'n', 'v', 'i'}, '<C-Tab>', '<cmd>tabnext<cr>')
 vim.keymap.set({'n', 'v', 'i'}, '<C-S-Tab>', '<cmd>tabprevious<cr>')
