c67ce5a85b2ebc1a971180c97db33a140ef8ce73
andriyDev <andriydzikh@gmail.com>
2025-03-14 17:55:33 -0700

    Set the "regular" options like line counts and tabstops.

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,17 @@
+local opt = vim.opt
+
+opt.scrolloff = 10
+opt.cc = { 100 }
+opt.wrap = false
+opt.signcolumn = 'yes'
+
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+
+opt.hlsearch = true
+opt.wildmode = 'list:longest'
+
+opt.number = true
+opt.relativenumber = true
+
