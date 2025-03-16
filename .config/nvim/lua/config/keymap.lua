6487da072bf9aa6f951cb273717b06d8fca2de80
andriyDev <andriydzikh@gmail.com>
2025-03-15 17:19:52 -0700

    Add keybindings for common LSP stuff.

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -5,3 +5,8 @@ keymap.set('', '<A-z>', '<cmd>set wrap!<cr>')
 
 keymap.set('', '<C-p>', '<cmd>Telescope frecency workspace=CWD<cr>')
 
+keymap.set('', 'K', function() vim.lsp.buf.hover() end)
+keymap.set('', '<F2>', function() vim.lsp.buf.rename() end)
+keymap.set('', '<F12>', function() vim.lsp.buf.definition() end)
+keymap.set('', '<C-Space>', function() vim.diagnostic.open_float() end)
+
