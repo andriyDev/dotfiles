b8a706f7277a47c42520445b95a32db1dc8d7a64
andriyDev <andriydzikh@gmail.com>
2025-03-15 21:14:26 -0700

    Make C-k trigger code actions.

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -9,5 +9,6 @@ keymap.set('', 'K', function() vim.lsp.buf.hover() end)
 keymap.set('', '<F2>', function() vim.lsp.buf.rename() end)
 keymap.set('', '<F12>', function() vim.lsp.buf.definition() end)
 keymap.set('', '<S-A-F12>', function() vim.lsp.buf.references() end)
+keymap.set('', '<C-k>', function() vim.lsp.buf.code_action() end)
 keymap.set('', '<C-Space>', function() vim.diagnostic.open_float() end)
 
