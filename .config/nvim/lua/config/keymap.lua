fd40dd99a7d09d0abe3ee8db4cd0a14812546cf9
andriyDev <andriydzikh@gmail.com>
2025-03-15 21:10:11 -0700

    Make S-A-F12 show references.

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -8,5 +8,6 @@ keymap.set('', '<C-p>', '<cmd>Telescope frecency workspace=CWD<cr>')
 keymap.set('', 'K', function() vim.lsp.buf.hover() end)
 keymap.set('', '<F2>', function() vim.lsp.buf.rename() end)
 keymap.set('', '<F12>', function() vim.lsp.buf.definition() end)
+keymap.set('', '<S-A-F12>', function() vim.lsp.buf.references() end)
 keymap.set('', '<C-Space>', function() vim.diagnostic.open_float() end)
 
